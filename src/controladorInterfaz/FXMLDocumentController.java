4106338918338e70a565adb447a76465c80a0446
moreno1998b <jorge_enrique.moreno@uao.edu.co>
2020-10-04 16:42:03 -0500

    cambios vis

--- src/controladorInterfaz/FXMLDocumentController.java
+++ src/controladorInterfaz/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
@@ -17,19 +18,30 @@ import javafx.scene.control.Label;
  * @author 57311
  */
 public class FXMLDocumentController implements Initializable {
-    
+
+    @FXML
+    private Button btnRombo;
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
     }
-    
+
+    @FXML
+    public void saludar(ActionEvent event) {
+//        System.out.println("You clicked me!");
+        System.out.println("hola mundo");
+//        label.setText("Hello World!");
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+//       btnRombo.getStylesheets().add("button");
         // TODO
-    }    
-    
+    }
+
 }
