965c07f4efea062593af74fdedbd7cbcbe1f2f06
moreno1998b <jorge_enrique.moreno@uao.edu.co>
2020-10-07 19:17:39 -0500

    Cambios visuales

--- src/controladorInterfaz/FXMLDocumentController.java
+++ src/controladorInterfaz/FXMLDocumentController.java
@@ -28,15 +28,15 @@ import modelo.Puntos2D;
  * @author 57311
  */
 public class FXMLDocumentController implements Initializable {
-
+    
     @FXML
     private Canvas canvas;
     @FXML
     private Button btnRombo;
-
+    
     @FXML
     private Button btnOctagono;
-
+    
     @FXML
     private Button btnPentagono;
     @FXML
@@ -49,103 +49,169 @@ public class FXMLDocumentController implements Initializable {
     private ColorPicker colorLineado;
     @FXML
     private ColorPicker colorRelleno;
-
+    
     @FXML
     private Label labelLineado;
-
+    
     @FXML
     private Label labelRelleno;
     private GraphicsContext lienzo;
     private Color c;
     private Color c2;
+    private Integer contadorH = 0;
+    private Integer contadorR=0;
     HashMap<String, LinkedList<Puntos2D>> mapFiguras;
     LinkedList<Puntos2D> listapuntosFigura;
     double coordx;
     double coordy;
     double[] arrayx;
     double[] arrayy;
-
+    
     @FXML
     private void handleButtonAction(ActionEvent event) {
-
+        
     }
-
-    @FXML
-    public void saludar(ActionEvent event) {
-//        System.out.println("You clicked me!");
-        System.out.println("hola mundo");
-//        label.setText("Hello World!");
+    
+     @FXML
+    private void crearRombo(MouseEvent event) {
+        LinkedList<Puntos2D> lista = new LinkedList<>();
+        LinkedList<Figura2D> listaf = new LinkedList<>();
+        
+        arrayx = new double[4];
+        arrayy = new double[4];
+        
+        lienzo.setLineWidth(3);
+        
+        int radio = 50;
+        
+        arrayx[0] = (coordx + radio);
+        arrayy[0] = (coordy);
+        
+        arrayx[1] = coordx + radio * Math.cos(2 * Math.PI / 6);
+        arrayy[1] = coordy + radio * Math.sin(2 * Math.PI / 6);
+        
+        arrayx[2] = coordx + radio * Math.cos(2 * 2 * Math.PI / 6);
+        arrayy[2] = coordy + radio * Math.sin(2 * 2 * Math.PI / 6);
+        
+        arrayx[3] = coordx + radio * Math.cos(3 * 2 * Math.PI / 6);
+        arrayy[3] = coordy + radio * Math.sin(3 * 2 * Math.PI / 6);
+        
+//        arrayx[4] = coordx + radio * Math.cos(4 * 2 * Math.PI / 6);
+//        arrayy[4] = coordy + radio * Math.sin(4 * 2 * Math.PI / 6);
+//        
+//        arrayx[5] = coordx + radio * Math.cos(5 * 2 * Math.PI / 6);
+//        arrayy[5] = coordy + radio * Math.sin(5 * 2 * Math.PI / 6);
+        lienzo.setStroke(c);
+        lienzo.strokePolygon(arrayx, arrayy, 4);
+        lienzo.setFill(c);
+        lienzo.setFill(c2);
+        lienzo.setStroke(c2);
+        lienzo.setLineWidth(1);
+//
+        lienzo.fillPolygon(arrayx, arrayx, 4);
+        lienzo.setFill(c2);
+        for (int i = 0; i < arrayx.length; i++) {
+            listapuntosFigura.add(new Puntos2D(arrayx[i], arrayy[i], c, c2));
+            mapFiguras.put("Rombo" + contadorR, listapuntosFigura);
+            contadorR++;
+            
+        }
+        
     }
-
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 //       btnRombo.getStylesheets().add("button");
+        listapuntosFigura = new LinkedList<>();
         lienzo = canvas.getGraphicsContext2D();
         mapFiguras = new HashMap<>();
         // TODO
     }
-
+    
     @FXML
     private void changeColorLineado(ActionEvent event) {
         c = colorLineado.getValue();
 //        c2 = colorRelleno.getValue();
 //       Color blue = new Color(c.getRed(), c.getGreen(), c.getBlue(), c.getOpacity());
         System.out.println(c);
+//        lienzo.setStroke(c);
     }
-
+    
     @FXML
     private void changeColorRelleno(ActionEvent event) {
 //        c = colorLineado.getValue();
         c2 = colorRelleno.getValue();
         System.out.println(c2);
     }
-
+    
     @FXML
     private void crearHexagono(MouseEvent event) {
         LinkedList<Puntos2D> lista = new LinkedList<>();
         LinkedList<Figura2D> listaf = new LinkedList<>();
-
+        
         arrayx = new double[6];
         arrayy = new double[6];
-
+        
+        lienzo.setLineWidth(3);
+        
+        int radio = 50;
+        
+        arrayx[0] = (coordx + radio);
+        arrayy[0] = (coordy);
+        
+        arrayx[1] = coordx + radio * Math.cos(2 * Math.PI / 6);
+        arrayy[1] = coordy + radio * Math.sin(2 * Math.PI / 6);
+        
+        arrayx[2] = coordx + radio * Math.cos(2 * 2 * Math.PI / 6);
+        arrayy[2] = coordy + radio * Math.sin(2 * 2 * Math.PI / 6);
+        
+        arrayx[3] = coordx + radio * Math.cos(3 * 2 * Math.PI / 6);
+        arrayy[3] = coordy + radio * Math.sin(3 * 2 * Math.PI / 6);
+        
+        arrayx[4] = coordx + radio * Math.cos(4 * 2 * Math.PI / 6);
+        arrayy[4] = coordy + radio * Math.sin(4 * 2 * Math.PI / 6);
+        
+        arrayx[5] = coordx + radio * Math.cos(5 * 2 * Math.PI / 6);
+        arrayy[5] = coordy + radio * Math.sin(5 * 2 * Math.PI / 6);
+        lienzo.setStroke(c);
+        lienzo.strokePolygon(arrayx, arrayy, 6);
+        lienzo.setFill(c);
+        lienzo.setFill(c2);
+        lienzo.setStroke(c2);
+        lienzo.setLineWidth(1);
 //
-//        listapuntosFigura = new LinkedList<>();
-//        double[] coordenadasx = {coordx + 200, x + 400, x + 450, x + 400, x + 200, x + 150};
-//        double[] coordenadasy = {y + 50, y + 50, y + 150, y + 250, y + 250, y + 150};
-//
-//        lienzo.setLineWidth(1);
-//        lienzo.setStroke(c);
-//        lienzo.strokePolygon(coordenadasx, coordenadasy, 6);
-//        lienzo.setStroke(c2);
-//
-//        for (int i = 0; i < coordenadasy.length; i++) {
-//            listapuntosFigura.add(new Puntos2D(coordenadasx[i], coordenadasy[i], colorRelleno.getValue(), colorLineado.getValue()));
-//
-//        }
-//        mapFiguras.put("Hexagono", listapuntosFigura);
+        lienzo.fillPolygon(arrayx, arrayx, 6);
+        lienzo.setFill(c2);
+        for (int i = 0; i < arrayx.length; i++) {
+            listapuntosFigura.add(new Puntos2D(arrayx[i], arrayy[i], c, c2));
+            mapFiguras.put("Hexagono" + contadorH, listapuntosFigura);
+            contadorH++;
+            
+        }
+        
     }
-
+    
     @FXML
     private void obtenerCoordenadas(MouseEvent event) {
         coordx = event.getX();
         coordy = event.getY();
         System.out.println(coordx + "-" + coordy);
     }
-
+    
     public double getCoordx() {
         return coordx;
     }
-
+    
     public void setCoordx(double coordx) {
         this.coordx = coordx;
     }
-
+    
     public double getCoordy() {
         return coordy;
     }
-
+    
     public void setCoordy(double coordy) {
         this.coordy = coordy;
     }
-
+    
 }
